48dbdc6e4128e2922a0bf2b5b3ee4c47c7a5102a
hemap <hemaprabha.rct@gmail.com>
2024-01-12 21:43:44 +0530

    four commit

--- src/components/VideoGallery.jsx
+++ src/components/VideoGallery.jsx
@@ -7,7 +7,7 @@ const VideoGallery = () => {
     const [data,ChangeData]=useState([])
 
     const fetchData=()=>{
-        axios.post("http://127.0.0.1:8000/subtitles/").then(
+        axios.post("http://127.0.0.1:8000/").then(
             (response)=>{
                 ChangeData(response.data)
             }
